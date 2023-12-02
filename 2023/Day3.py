59717bf35f4715927ff00487030f76beb0e8df0f
Scott Boston <scott.boston@yahoo.com>
2023-12-02 15:57:01 -0600

    Add blank Day 3

---
+++ 2023/Day3.py
@@ -0,0 +1,20 @@
+import aoc_utils.myconfig as utils
+import pandas as pd
+from io import StringIO
+
+
+def part1():
+    input_data = utils.get_data(year=2023, day=3)
+    results = None
+    return results
+
+
+def part2():
+    input_data = utils.get_data(year=2023, day=3)
+    results = None
+    return results
+
+
+if __name__ == "__main__":
+    print(f"{part1()=}")
+    print(f"{part2()=}")
