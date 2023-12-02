efb2c2d12a57ed5c936b2ba83461cf0a82d10db3
Scott Boston <scott.boston@yahoo.com>
2023-12-01 23:46:03 -0600

    AOC 2023 Day 2

--- 2023/Day2.py
+++ 2023/Day2.py
@@ -5,20 +5,41 @@ from io import StringIO
 
 def part1():
     input_data = utils.get_data(year=2023, day=2)
-    df = pd.read_csv(StringIO(input_data), sep=';', header=None, names=range(10))
+    df = pd.read_csv(StringIO(input_data), sep=";", header=None, names=range(10))
     df = df.stack()
-    df = pd.concat([(df.str.extractall(f'(?P<{c}>\d+) {c}').astype(int).le(n).groupby(level=0).all())
-                for c, n in zip(['red', 'green', 'blue'],[12,13,14])], axis=1).all(1)
+    df = pd.concat(
+        [
+            (
+                df.str.extractall(f"(?P<{c}>\d+) {c}")
+                .astype(int)
+                .le(n)
+                .groupby(level=0)
+                .all()
+            )
+            for c, n in zip(["red", "green", "blue"], [12, 13, 14])
+        ],
+        axis=1,
+    ).all(1)
     df.index = df.index + 1
     results = sum(df.where(df).dropna().index.to_list())
     return results
 
+
 def part2():
     input_data = utils.get_data(year=2023, day=2)
-    df = pd.read_csv(StringIO(input_data), sep=';', header=None, names=range(10))
+    df = pd.read_csv(StringIO(input_data), sep=";", header=None, names=range(10))
     df = df.stack()
-    df = pd.concat([(df.str.extractall(f'(?P<{c}>\d+) {c}').astype(int))
-                    for c, n in zip(['red', 'green', 'blue'], [12, 13, 14])], axis=1).groupby(level=0).max()
+    df = (
+        pd.concat(
+            [
+                (df.str.extractall(f"(?P<{c}>\d+) {c}").astype(int))
+                for c, n in zip(["red", "green", "blue"], [12, 13, 14])
+            ],
+            axis=1,
+        )
+        .groupby(level=0)
+        .max()
+    )
     results = df.prod(1).sum()
     return results
 
