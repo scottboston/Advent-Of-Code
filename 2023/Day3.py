6619844331a7f7cf953d0001265a0fb8beb82fb3
Scott Boston <scott.boston@yahoo.com>
2023-12-03 00:39:22 -0600

    AOC 2023 Day 3 part 1

--- 2023/Day3.py
+++ 2023/Day3.py
@@ -1,11 +1,26 @@
 import aoc_utils.myconfig as utils
-import pandas as pd
-from io import StringIO
+import re
 
 
 def part1():
     input_data = utils.get_data(year=2023, day=3)
-    results = None
+    i = input_data.split()
+    num = []
+    for n, l in enumerate(i):
+        matches = re.finditer("\d+", l)
+        for m in matches:
+            s = m.start()
+            e = m.end()
+            mm = l[s:e]
+            for lc in range(-1, 2):
+                if n + lc >= 0 and n + lc < len(i):
+                    s = s + 1 if s == 0 else s
+                    e = e + 1 if e == len(l) else e
+                    checkstr = i[n + lc][s - 1 : e + 1]
+                    if isinstance(re.search("[^\d.]", checkstr), re.Match):
+                        num.append(mm)
+                        break
+    results = sum(int(n) for n in num)
     return results
 
 
