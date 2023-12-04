749c7d05503d4311d32efc0942def2b85b7df4af
Scott Boston <scott.boston@yahoo.com>
2023-12-04 09:20:42 -0600

    Update Day4.py
    Add Day 4 Part 1 and Part 2

--- 2023/Day4.py
+++ 2023/Day4.py
@@ -4,13 +4,19 @@ import re
 
 def part1():
     input_data = utils.get_data(year=2023, day=3)
-    results = None
+    results = sum(int(2**(len(set.intersection(*map(lambda x: set(x.split()), l[8:].split(" | "))))-1)) for l in input_data.split('\n'))
     return results
 
 
 def part2():
     input_data = utils.get_data(year=2023, day=3)
-    results = None
+    lines = input_data.split('\n')
+    card_count = {n:1 for n in range(1, len(lines)+1)}
+    for n, l in enumerate(lines, 1):
+        matches = len(set.intersection(*map(lambda x: set(x.split()), l[8:].split(" | "))))
+        for c in range(1, matches+1):
+            card_count[c+n] += card_count[n]
+    results = sum(card_count.values())
     return results
 
 
