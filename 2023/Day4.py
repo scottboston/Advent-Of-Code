b1beeb8ff41065d48e409e8b148c7e9111bfc032
Scott Boston <scott.boston@yahoo.com>
2023-12-04 21:02:57 -0600

    Fix Day 4 get_data parameters

--- 2023/Day4.py
+++ 2023/Day4.py
@@ -3,19 +3,32 @@ import re
 
 
 def part1():
-    input_data = utils.get_data(year=2023, day=3)
-    results = sum(int(2**(len(set.intersection(*map(lambda x: set(x.split()), l[8:].split(" | "))))-1)) for l in input_data.split('\n'))
+    input_data = utils.get_data(year=2023, day=4)
+    results = sum(
+        int(
+            2
+            ** (
+                len(
+                    set.intersection(*map(lambda x: set(x.split()), l[8:].split(" | ")))
+                )
+                - 1
+            )
+        )
+        for l in input_data.split("\n")
+    )
     return results
 
 
 def part2():
-    input_data = utils.get_data(year=2023, day=3)
-    lines = input_data.split('\n')
-    card_count = {n:1 for n in range(1, len(lines)+1)}
+    input_data = utils.get_data(year=2023, day=4)
+    lines = input_data.split("\n")
+    card_count = {n: 1 for n in range(1, len(lines) + 1)}
     for n, l in enumerate(lines, 1):
-        matches = len(set.intersection(*map(lambda x: set(x.split()), l[8:].split(" | "))))
-        for c in range(1, matches+1):
-            card_count[c+n] += card_count[n]
+        matches = len(
+            set.intersection(*map(lambda x: set(x.split()), l[8:].split(" | ")))
+        )
+        for c in range(1, matches + 1):
+            card_count[c + n] += card_count[n]
     results = sum(card_count.values())
     return results
 
