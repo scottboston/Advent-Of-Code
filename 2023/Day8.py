547cea3a07b653e90e31eff4595f64d0f9597779
Scott Boston <scott.boston@yahoo.com>
2023-12-08 01:10:07 -0600

    Day 8 Part 2

--- 2023/Day8.py
+++ 2023/Day8.py
@@ -3,6 +3,7 @@ import pandas as pd
 from io import StringIO
 from collections import Counter
 import numpy as np
+import math
 
 
 def part1():
@@ -18,21 +19,42 @@ def part1():
     dfs = df.stack()
     row = "AAA"
     i = 0
-    print(len(I))
     while row != "ZZZ":
         r = i % len(I)
         i += 1
         row = dfs.loc[row, I[r]]
-        if i % 5_000 == 0:
-            print(f"{i=} and {row=}")
-    print(f"total {i=}")
+    results = i
+
+    return results
+
+
+def part2_func(row, dfs):
+    I = "LRRLRRLRRLRRRLRRLRRRLRRLRRRLRLRLLRLRLRRLLLRLRLRRRLRRLRLRRRLRRLRRLRRLLLRRLRRRLRRRLRLLRRLRLLRRLRRRLRRLRLRRRLRLRLRRLRLRRRLLRRRLLRRRLRLRRRLRRLLRRLRRRLRRLRRLLRRLRRLRRRLLLRRRLRRLRRLRRLRLRRRLRRLLLLRLRRLRRRLRLLRRLRLLRRLRRRLRRRLRRRLLRRLRRLRRLRRRLRRLRRRLLRLRRRLRRRLRRRLLRRRLRRLRRRR"
+    i = 0
+    while row[2] != "Z":
+        r = i % len(I)
+        i += 1
+        row = dfs.loc[row, I[r]]
     results = i
 
     return results
 
 
 def part2():
-    results = None
+    input_data = utils.get_data(year=2023, day=8)
+    df = pd.read_csv(StringIO(input_data), header=None, skiprows=2)
+    df[["A", "L"]] = df[0].str.split("=|\(", expand=True)[[0, 2]]
+    df["R"] = df[1].str.strip("\)")
+    df = df.set_index("A")[["L", "R"]]
+    df.index = df.index.str.strip()
+    df["L"] = df["L"].str.strip()
+    df["R"] = df["R"].str.strip()
+    dfs = df.stack()
+    rows = df[dfs.index.levels[0].str[2] == "A"].index.tolist()
+    i = []
+    for row in rows:
+        i.append(part2_func(row, dfs))
+    results = math.lcm(*i)
     return results
 
 
