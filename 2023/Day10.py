332a480813d0b50bb5d6d648a9916407cd8932a4
Scott Boston <scott.boston@yahoo.com>
2023-12-11 18:05:37 -0600

    Adding Day 10 part 1

---
+++ 2023/Day10.py
@@ -0,0 +1,63 @@
+import aoc_utils.myconfig as utils
+import numpy as np
+from io import StringIO
+import pandas as pd
+import networkx as nx
+
+
+def part1():
+    input_data = utils.get_data(year=2023, day=10)
+    a = np.array([[*l] for l in input_data.splitlines()])
+    df = pd.DataFrame(a)
+
+    # Add borders around layout
+    df.loc[df.shape[0], :] = "."
+    df.loc[-1, :] = "."
+
+    df.loc[:, df.shape[1]] = "."
+    df.loc[:, -1] = "."
+
+    # Setup directions and pipe segment identifications
+    N = (-1, 0)
+    W = (0, -1)
+    S = (1, 0)
+    E = (0, 1)
+    direction = [N, W, S, E]
+    to_pipes = {
+        "F": [S, E],
+        "|": [N, S],
+        "-": [W, E],
+        "J": [N, W],
+        "7": [S, W],
+        "S": [N, W, S, E],
+        "L": [N, E],
+        ".": " ",
+    }
+
+    r, c = np.where(df.ne("."))
+    pipes = dict(enumerate(zip(r, c)))
+    ctop = {v: k for k, v in pipes.items()}
+
+    # Use networkx for graph layout
+    G = nx.Graph()
+    for id, c in pipes.items():
+        for d in to_pipes[df.loc[c]]:
+            o = c[0] + d[0], c[1] + d[1]
+            if df.loc[o] != "." and tuple(map(np.negative, (d))) in to_pipes[df.loc[o]]:
+                G.add_edge(ctop[c], ctop[o])
+
+    sloc = ctop[tuple([e[0] for e in np.where(df == "S")])]
+
+    results = max(nx.single_source_shortest_path_length(G, sloc).values())
+    return results
+
+
+def part2():
+    input_data = utils.get_data(year=2023, day=10)
+    results = None
+    return results
+
+
+if __name__ == "__main__":
+    print(f"{part1()=}")
+    print(f"{part2()=}")
