f8cf106869aeacfed9c868a5a679fcbd2d244052
Scott Boston <scott.boston@yahoo.com>
2023-12-13 22:34:16 -0600

    Add Day 13 Part 2

--- 2023/Day13.py
+++ 2023/Day13.py
@@ -37,6 +37,28 @@ def split_index(max, s):
     return np.arange(max)[ll:hl], np.arange(max)[hl:hh]
 
 
+def test_row_mirror_2(arr):
+    cs = np.where((arr[:-1, :] != arr[1:, :]).sum(1) < 2)[0]
+    for c in cs.tolist():
+        i = c + 1
+        il, ih = split_index(arr.shape[0], i)
+        tr = arr[il, :] != np.flipud(arr[ih, :])
+        if tr.sum() == 1:
+            return c + 1
+    return 0
+
+
+def test_col_mirror_2(arr):
+    cs = np.where((arr[:, :-1] != arr[:, 1:]).sum(0) < 2)[0]
+    for c in cs.tolist():
+        i = c + 1
+        il, ih = split_index(arr.shape[1], i)
+        tr = arr[:, il] != np.fliplr(arr[:, ih])
+        if tr.sum() == 1:
+            return c + 1
+    return 0
+
+
 def part1():
     input_data = utils.get_data(year=2023, day=13)
     frames = {}
@@ -63,7 +85,25 @@ def part1():
 
 def part2():
     input_data = utils.get_data(year=2023, day=13)
-    results = None
+    frames = {}
+    frame_number = 0
+
+    for lines in input_data.splitlines():
+        if len(lines) == 0:
+            frame_number += 1
+            continue
+        if frames.get(frame_number):
+            frames[frame_number].append(lines)
+        else:
+            frames[frame_number] = [lines]
+
+    r_total = 0
+    c_total = 0
+    for _, frame in frames.items():
+        arr = np.array([[*line] for line in frame], dtype=object)
+        r_total += test_row_mirror_2(arr)
+        c_total += test_col_mirror_2(arr)
+    results = r_total * 100 + c_total
     return results
 
 
