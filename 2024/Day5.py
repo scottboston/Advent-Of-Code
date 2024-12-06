a8cffb07dabc0cec15da81606af653717aad80e9
Scott Boston <scott.boston@yahoo.com>
2024-12-05 22:07:25 -0600

    Day 5 part 2

--- 2024/Day5.py
+++ 2024/Day5.py
@@ -46,9 +46,26 @@ def part1():
         l = l.split(",")
         if all(G.has_successor(p, c) for p, c in combinations(l, 2)):
             mid.append(l[len(l) // 2])
-    print(mid)
+    return sum(map(int, mid))
+
+
+def part2():
+    G = nx.DiGraph()
+    for l in page_order:
+        G.add_edge(l[0], l[1])
+    print(G)
+    mid = []
+    for l in print_order:
+        l = l.split(",")
+        if all(G.has_successor(p, c) for p, c in combinations(l, 2)):
+            pass
+        else:
+            g = G.subgraph(l)
+            l = list(filter(lambda x: x in l, nx.topological_sort(g)))
+            mid.append(l[len(l) // 2])
     return sum(map(int, mid))
 
 
 if __name__ == "__main__":
-    print(f"{part1()=}")
+    # print(f"{part1()=}")
+    print(f"{part2()=}")
