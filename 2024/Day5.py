3ede9e967a1a584e75c28b127bc432d9e38ecf48
Scott Boston <scott.boston@yahoo.com>
2024-12-05 22:08:25 -0600

    Day 5 Complete

--- 2024/Day5.py
+++ 2024/Day5.py
@@ -2,35 +2,6 @@ import aoc_utils.myconfig as utils
 import networkx as nx
 from itertools import combinations
 
-input_data = """47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47"""
-
 input_data = utils.get_data(year=2024, day=5)
 page_order = [l.split("|") for l in input_data.splitlines() if "|" in l]
 print_order = [l for l in input_data.splitlines() if "," in l]
@@ -67,5 +38,5 @@ def part2():
 
 
 if __name__ == "__main__":
-    # print(f"{part1()=}")
+    print(f"{part1()=}")
     print(f"{part2()=}")
