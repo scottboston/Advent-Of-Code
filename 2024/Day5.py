4216ec48930b1a5cd624005dd371b22690ac69a1
Scott Boston <scott.boston@yahoo.com>
2024-12-07 10:30:44 -0600

    Day 5 Refactor

--- 2024/Day5.py
+++ 2024/Day5.py
@@ -3,25 +3,25 @@ import networkx as nx
 from itertools import combinations
 
 input_data = utils.get_data(year=2024, day=5)
-page_order = [l.split("|") for l in input_data.splitlines() if "|" in l]
-print_order = [l for l in input_data.splitlines() if "," in l]
+page_order = [line.split("|") for line in input_data.splitlines() if "|" in line]
+print_order = [line for line in input_data.splitlines() if "," in line]
 
 
 def part1_and_part2():
     G = nx.DiGraph()
-    for l in page_order:
-        G.add_edge(l[0], l[1])
+    for pages in page_order:
+        G.add_edge(pages[0], pages[1])
     print(G)
     part1_mid = []
     part2_mid = []
-    for l in print_order:
-        l = l.split(",")
-        if all(G.has_successor(p, c) for p, c in combinations(l, 2)):
-            part1_mid.append(l[len(l) // 2])
+    for pages in print_order:
+        pages = pages.split(",")
+        if all(G.has_successor(p, c) for p, c in combinations(pages, 2)):
+            part1_mid.append(pages[len(pages) // 2])
         else:
-            g = G.subgraph(l)
-            l = list(filter(lambda x: x in l, nx.topological_sort(g)))
-            part2_mid.append(l[len(l) // 2])
+            g = G.subgraph(pages)
+            pages = list(filter(lambda page: page in pages, nx.topological_sort(g)))
+            part2_mid.append(pages[len(pages) // 2])
     return sum(map(int, part1_mid)), sum(map(int, part2_mid))
 
 
