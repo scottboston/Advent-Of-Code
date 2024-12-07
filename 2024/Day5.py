9db3d1de61c4dc49389d44668ecf55358409276b
Scott Boston <scott.boston@yahoo.com>
2024-12-07 10:25:39 -0600

    Day 5 Refactor

--- 2024/Day5.py
+++ 2024/Day5.py
@@ -7,36 +7,23 @@ page_order = [l.split("|") for l in input_data.splitlines() if "|" in l]
 print_order = [l for l in input_data.splitlines() if "," in l]
 
 
-def part1():
+def part1_and_part2():
     G = nx.DiGraph()
     for l in page_order:
         G.add_edge(l[0], l[1])
     print(G)
-    mid = []
+    part1_mid = []
+    part2_mid = []
     for l in print_order:
         l = l.split(",")
         if all(G.has_successor(p, c) for p, c in combinations(l, 2)):
-            mid.append(l[len(l) // 2])
-    return sum(map(int, mid))
-
-
-def part2():
-    G = nx.DiGraph()
-    for l in page_order:
-        G.add_edge(l[0], l[1])
-    print(G)
-    mid = []
-    for l in print_order:
-        l = l.split(",")
-        if all(G.has_successor(p, c) for p, c in combinations(l, 2)):
-            pass
+            part1_mid.append(l[len(l) // 2])
         else:
             g = G.subgraph(l)
             l = list(filter(lambda x: x in l, nx.topological_sort(g)))
-            mid.append(l[len(l) // 2])
-    return sum(map(int, mid))
+            part2_mid.append(l[len(l) // 2])
+    return sum(map(int, part1_mid)), sum(map(int, part2_mid))
 
 
 if __name__ == "__main__":
-    print(f"{part1()=}")
-    print(f"{part2()=}")
+    print(f"{part1_and_part2()=}")
