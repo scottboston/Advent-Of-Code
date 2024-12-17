865feaac9e2c7a7466c2e1e3a15577bbc17e8459
Scott Boston <scott.boston@yahoo.com>
2024-12-16 22:43:52 -0600

    Day 16 Part I done.

--- 2024/Day16.py
+++ 2024/Day16.py
@@ -1,8 +1,6 @@
-##### Code Not Updated with solution ####
-
 import aoc_utils.myconfig as utils
 import numpy as np
-import nx_cugraph as nx
+import networkx as nx
 import pandas as pd
 from tqdm import tqdm
 
@@ -49,58 +47,74 @@ def get_input_data(testdata: bool = True) -> str:
 
 
 def part1():
-    a = np.array([[*l] for l in get_input_data(False).strip('\n').splitlines()])
+    a = np.array([[*l] for l in get_input_data(False).strip("\n").splitlines()])
     df = pd.DataFrame(a)
     dfr, dfc = df.shape
-    print(f'{df.shape=}')
-    G = nx.DiGraph()
+    directions = ["^", ">", "v", "<"]
+    G = nx.Graph()
 
-    dft = df.isin(['.', 'S', 'E'])
+    dft = df.isin([".", "S", "E"])
 
-    print('Start Shift1...')
     r, c = np.where(dft == dft.shift())
     for i in zip(r, c):
-        # print(f'{i[0] - 1, i[1]}->{i}')
-        G.add_edge((i[0] - 1, i[1]), i)
+        [
+            G.add_edge(
+                (i[0], i[1], directions[d]),
+                (i[0], i[1], directions[(d + 1) % 4]),
+                weight=1000,
+            )
+            for d in range(4)
+        ]
+        G.add_edge((i[0] - 1, i[1], "v"), (i[0], i[1], "v"), weight=1)
 
-    print('Start Shift2...')
     r, c = np.where(dft == dft.shift(-1))
     for i in zip(r, c):
-        # print(f'({i[0] + 1},{i[1]})->{i}')
-        G.add_edge((i[0] + 1, i[1]), i)
+        [
+            G.add_edge(
+                (i[0], i[1], directions[d]),
+                (i[0], i[1], directions[(d + 1) % 4]),
+                weight=1000,
+            )
+            for d in range(4)
+        ]
+        G.add_edge((i[0] + 1, i[1], "^"), (i[0], i[1], "^"), weight=1)
 
-    print('Start Shift3...')
     r, c = np.where(dft == dft.shift(-1, axis=1))
     for i in zip(r, c):
-        #         print(f'{i[0], i[1] + 1}->{i}')
-        G.add_edge((i[0], i[1] + 1), i)
+        [
+            G.add_edge(
+                (i[0], i[1], directions[d]),
+                (i[0], i[1], directions[(d + 1) % 4]),
+                weight=1000,
+            )
+            for d in range(4)
+        ]
+        G.add_edge((i[0], i[1] + 1, "<"), (i[0], i[1], "<"), weight=1)
 
-    print('Shift Shift4...')
     r, c = np.where(dft == dft.shift(axis=1))
     for i in zip(r, c):
         #         print(f'{i[0], i[1] - 1}->{i}')
-        G.add_edge((i[0], i[1] - 1), i)
+        [
+            G.add_edge(
+                (i[0], i[1], directions[d]),
+                (i[0], i[1], directions[(d + 1) % 4]),
+                weight=1000,
+            )
+            for d in range(4)
+        ]
+        G.add_edge((i[0], i[1] - 1, ">"), (i[0], i[1], ">"), weight=1)
 
-    min_score = np.inf
-    print('Start getting all paths')
-    paths = list(nx.all_simple_edge_paths(G,(dfr-2,1), (1,dfc-2)))
-    print('End get paths')
+    [G.add_edge((1, dfc - 2, directions[d]), (1, dfc - 2), weight=0) for d in range(4)]
 
-    print(f'{len(paths)=}')
+    print(G)
+    paths = nx.single_source_dijkstra(
+        G, source=(dfr - 2, 1, ">"), target=(1, dfc - 2), weight="weight"
+    )
 
-    for path in tqdm(paths, ):
-        dir_in = None
-        score = 0
-        for c, n in path:
-            dir_out = 'r' if c[0] == n[0] else 'c'
-            score += 1 if dir_out == dir_in else 1001
-            if score >= min_score:
-                break
-            dir_in = dir_out
-        min_score = min(min_score, score)
+    result = paths[0]
 
-    result = min_score
     return result
 
-if __name__ == '__main__':
-    print(f'{part1()=}')
+
+if __name__ == "__main__":
+    print(f"{part1()=}")
