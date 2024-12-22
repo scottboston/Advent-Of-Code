7a0e8cf9275ad159b6b0f8d8c447ab882200a0eb
Scott Boston <scott.boston@yahoo.com>
2024-12-21 22:57:33 -0600

    Day 19 Part I

---
+++ 2015/Day11.py
@@ -0,0 +1,15 @@
+import aoc_utils.myconfig as utils
+from loguru import logger
+from string import ascii_lowercase
+
+d = dict(enumerate(ascii_lowercase, 1))
+
+
+def part1():
+    input = "hepxcrrq"
+
+    return
+
+
+if __name__ == "__main__":
+    logger.info(f"{part1()=}")
