0dd337684d6d033b66543f5b3b38743908f98ac2
Scott Boston <scott.boston@yahoo.com>
2025-08-23 14:51:13 -0500

    Add Year 2015 Day 13 part 2

--- 2015/Day13.py
+++ 2015/Day13.py
@@ -5,7 +5,7 @@ from itertools import permutations
 
 def part1():
     input_data = utils.get_data(year=2015, day=13)
-    dd = {'gain': 1, 'lose': -1}
+    dd = {"gain": 1, "lose": -1}
     table = []
     info = {}
 
@@ -13,24 +13,30 @@ def part1():
         pat = r"(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)"
         g = re.findall(pat, l)[0]
         table.append(g)
-        info[g[0] + ':' + g[3]] = int(dd[g[1]]) * int(g[2])
+        info[g[0] + ":" + g[3]] = int(dd[g[1]]) * int(g[2])
 
     people = set([p[0] for p in table])
     maxhap = -99_999
     for settings in permutations(people, len(people)):
         # print(settings)
-        hap = sum(info[settings[i] + ':' + settings[i + 1]] for i in range(len(settings) - 1))
-        hap += info[settings[0] + ':' + settings[len(settings) - 1]]
-        hapr = sum(info[settings[i] + ':' + settings[i - 1]] for i in range(len(settings) - 1, 0, -1))
-        hapr += info[settings[len(settings) - 1] + ':' + settings[0]]
+        hap = sum(
+            info[settings[i] + ":" + settings[i + 1]] for i in range(len(settings) - 1)
+        )
+        hap += info[settings[0] + ":" + settings[len(settings) - 1]]
+        hapr = sum(
+            info[settings[i] + ":" + settings[i - 1]]
+            for i in range(len(settings) - 1, 0, -1)
+        )
+        hapr += info[settings[len(settings) - 1] + ":" + settings[0]]
         total_happiness = hap + hapr
         # print(f'{hap=} + {hapr=} = {hap+hapr}')
         maxhap = total_happiness if total_happiness > maxhap else maxhap
     return maxhap
 
+
 def part2():
     input_data = utils.get_data(year=2015, day=13)
-    dd = {'gain': 1, 'lose': -1}
+    dd = {"gain": 1, "lose": -1}
     table = []
     info = {}
 
@@ -38,27 +44,32 @@ def part2():
         pat = r"(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)"
         g = re.findall(pat, l)[0]
         table.append(g)
-        info[g[0] + ':' + g[3]] = int(dd[g[1]]) * int(g[2])
+        info[g[0] + ":" + g[3]] = int(dd[g[1]]) * int(g[2])
 
     people = set([p[0] for p in table])
 
     for p in people:
-        info[p + ':Scott'] = 0
-        info['Scott:'+ p] = 0
-    people.add('Scott')
+        info[p + ":Scott"] = 0
+        info["Scott:" + p] = 0
+    people.add("Scott")
 
     maxhap = -99_999
     for settings in permutations(people, len(people)):
         # print(settings)
-        hap = sum(info[settings[i] + ':' + settings[i + 1]] for i in range(len(settings) - 1))
-        hap += info[settings[0] + ':' + settings[len(settings) - 1]]
-        hapr = sum(info[settings[i] + ':' + settings[i - 1]] for i in range(len(settings) - 1, 0, -1))
-        hapr += info[settings[len(settings) - 1] + ':' + settings[0]]
+        hap = sum(
+            info[settings[i] + ":" + settings[i + 1]] for i in range(len(settings) - 1)
+        )
+        hap += info[settings[0] + ":" + settings[len(settings) - 1]]
+        hapr = sum(
+            info[settings[i] + ":" + settings[i - 1]]
+            for i in range(len(settings) - 1, 0, -1)
+        )
+        hapr += info[settings[len(settings) - 1] + ":" + settings[0]]
         total_happiness = hap + hapr
         # print(f'{hap=} + {hapr=} = {hap+hapr}')
         maxhap = total_happiness if total_happiness > maxhap else maxhap
     return maxhap
 
 
-if __name__ == '__main__':
-    print(f'{part2()}')
+if __name__ == "__main__":
+    print(f"{part2()}")
