78dc83070900feae7a04448db348658ae941a153
Scott Boston <scott.boston@yahoo.com>
2025-09-05 18:06:36 -0500

    Add Year 2015 Day 15 Part 1 and 2

--- 2015/Day11.py
+++ 2015/Day11.py
@@ -1,15 +1,58 @@
 import aoc_utils.myconfig as utils
-from loguru import logger
 from string import ascii_lowercase
 
-d = dict(enumerate(ascii_lowercase, 1))
+def increment_password(password):
+    chars = list(password)
+    i = len(chars) - 1
+    while i >= 0:
+        if chars[i] == 'z':
+            chars[i] = 'a'
+            i -= 1
+        else:
+            chars[i] = chr(ord(chars[i]) + 1)
+            break
+    return ''.join(chars)
 
+def is_valid_password(password):
+    # Check for forbidden letters
+    if any(c in password for c in 'iol'):
+        return False
+    
+    # Check for increasing straight of 3 letters
+    has_straight = any(ord(password[i]) == ord(password[i+1]) - 1 == ord(password[i+2]) - 2 
+                      for i in range(len(password) - 2))
+    if not has_straight:
+        return False
+    
+    # Check for at least two different pairs
+    pairs = set()
+    i = 0
+    while i < len(password) - 1:
+        if password[i] == password[i+1]:
+            pairs.add(password[i])
+            i += 2  # Skip next char to avoid overlapping
+        else:
+            i += 1
+    
+    return len(pairs) >= 2
 
 def part1():
-    input = "hepxcrrq"
+    password = "hepxcrrq"
+    
+    while True:
+        password = increment_password(password)
+        if is_valid_password(password):
+            return password
 
-    return
+def part2():
+    password = part1()
+    
+    while True:
+        password = increment_password(password)
+        if is_valid_password(password):
+            return password
 
 
 if __name__ == "__main__":
-    logger.info(f"{part1()=}")
+    print(f"{part1()=}")
+    print(f"{part2()=}")
